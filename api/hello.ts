77d72b4f54fb46350fa54cbf7ecf8ae4bebc28a6
trendSetters01 <trendycrypto01@gmail.com>
2023-12-03 06:32:27 -0500

    init

--- api/hello.ts
+++ api/hello.ts
@@ -1,8 +1,18 @@
-import type { VercelRequest, VercelResponse } from '@vercel/node'
+import type { VercelRequest, VercelResponse } from "@vercel/node";
+import { User, AddressSet } from "../db/models";
 
 export default function handler(req: VercelRequest, res: VercelResponse) {
-  const { name = 'World' } = req.query
+  User.sync()
+    .then(() => console.log("User table created or successfully verified"))
+    .catch(console.error);
+
+  AddressSet.sync()
+    .then(() =>
+      console.log("AddressSet table created or successfully verified")
+    )
+    .catch(console.error);
+  const { name = "World" } = req.query;
   return res.json({
     message: `Hello ${name}!`,
-  })
+  });
 }
---
+++ db/models/stakingSet.ts
@@ -0,0 +1,70 @@
+import { DB, DataTypes } from "../config/index.js";
+
+const StakingSet = DB.define("StakingSet", {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  userId: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    references: {
+      model: 'User',
+      key: 'userId'
+    }
+  },
+  addressId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+      model: 'AddressSet',
+      key: 'id'
+    }
+  },
+  duration: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    validate: {
+      isInt: true,
+      min: 1
+    }
+  },
+  durationType: {
+    type: DataTypes.ENUM('days', 'weeks', 'months'),
+    allowNull: false,
+    defaultValue: 'weeks'
+  },
+  startDate: {
+    type: DataTypes.DATE,
+    allowNull: false,
+    defaultValue: DataTypes.NOW
+  },
+  endDate: {
+    type: DataTypes.DATE,
+    allowNull: true
+  },
+  status: {
+    type: DataTypes.ENUM('active', 'completed', 'cancelled'),
+    allowNull: false,
+    defaultValue: 'active'
+  },
+  boostId: {
+    type: DataTypes.INTEGER,
+    allowNull: true,
+    references: {
+      model: 'RoleBoost',
+      key: 'id'
+    }
+  },
+  createdAt: {
+    type: DataTypes.DATE,
+    defaultValue: DataTypes.NOW
+  },
+  updatedAt: {
+    type: DataTypes.DATE,
+    defaultValue: DataTypes.NOW
+  }
+});
+
+export { StakingSet };
